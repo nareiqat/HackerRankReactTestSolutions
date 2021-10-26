b04c3149e8bd97546e2fdf3817ce0b93f18485cf
nareiqat <nabeelareiqat@gmail.com>
2021-10-25 20:16:09 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,33 +3,44 @@ import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
+import { useState, useEffect } from 'react';
 
 const title = "Sorting Articles";
 
 function App({articles}) {
-    const [sortedby, setSortedBy] = React.useState(0);
-    let refinedArticles = function(){
-        if(sortedby === 0){
-            let r =   articles.sort((a,b) => (b.upvotes > a.upvotes) ? 1 : ((a.upvotes > b.upvotes) ? -1 : 0));
-            return r;
-        }else{
-            let r =   articles.sort((a,b) => (new Date(b.date) > new Date(a.date)) ? 1 : ((new Date(a.date) > new Date(b.date)) ? -1 : 0));
-            return r;
-        }
+   
+    // const setSortedByUp = articles.sort((a,b) => (b.upvotes > a.upvotes) ? 1 : ((a.upvotes > b.upvotes) ? -1 : 0))
+    const [sortedArticles, setSortedArticles] = useState(articles)
+
+    
+    useEffect(() => {
+        sortedByUpvotes()
+    })
+
+    const sortedByUpvotes = () => {
+        const arr = [...sortedArticles]
+        setSortedArticles(arr.sort((a,b) => (b.upvotes > a.upvotes) ? 1 : ((a.upvotes > b.upvotes) ? -1 : 0)))
     }
-    articles  = refinedArticles();
+
+    const sortedByDate = () => {
+        const arr = [...sortedArticles]
+        setSortedArticles( arr.sort((a,b) => (b.date > a.date) ? 1 : ((a.date > b.date) ? -1 : 0)))
+    }
+
+
     
 
+    
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button onClick={()=>setSortedBy(0)} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button onClick={()=>setSortedBy(1)} data-testid="most-recent-link" className="small">Most Recent</button>
+                <button onClick={sortedByUpvotes} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+                <button onClick={sortedByDate} data-testid="most-recent-link" className="small">Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={sortedArticles}/>
         </div>
     );
 
